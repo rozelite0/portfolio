bc964e6ab4ee4454aaad6853a95c72127ec20c07
rozelite0 <rozelite0@gmail.com>
2020-03-09 00:56:46 +0000

    Update page style

--- system.js
+++ system.js
@@ -38,3 +38,70 @@ MYWORK_WORDCOUNT.addEventListener('mouseout', () => {
     APP_OVERVIEW.innerHTML = "";
 }, false);
 
+//JQuery
+
+$(function () {
+    //グローバルナビの座標を取得
+    var offset = $('#globalNavi').offset();
+
+    /* スクロールイベント */
+    $(window).scroll(function () {
+        //scrollTopで現在のスクロールの縦位置を取得
+        //現在の縦位置とグローバルの縦座標位置を比較して、スクロール位置がグローバルナビより下になった場合、addclass
+        if ($(window).scrollTop() > offset.top) {
+            $('#globalNavi').addClass('fixed');
+            //グローバルナビより上だったらclassを削除    
+        } else {
+            $('#globalNavi').removeClass('fixed');
+        }
+    });
+    /* */
+});
+
+//ナビのリンクをクリックしたとき、スムーススクロールする
+$(function () {
+    //#で始まるリンクをクリックしたとき実行される
+    $('a[href^="#"]').click(function () {
+        //数字が増えるほど遅くなる
+        var speed = 550;
+        var href = $(this).attr("href");
+        var target = $(href == "#" || href == "" ? 'html' : href);
+        var position = target.offset().top;
+        $("html, body").animate({ scrollTop: position }, speed, "swing");
+        return false;
+    });
+});
+
+//スクロールすると画像も少し動く
+$(window).on('scroll', function () {
+
+    var scrollTop = $(window).scrollTop();
+    var bgPosition = scrollTop / 5; //スクロール後のポジションを指定（値を大きくすると移動距離が小さくなる）
+
+    if (bgPosition) {
+        $('#imageScroll').css('background-position', 'center top -' + bgPosition + 'px');
+    }
+});
+
+//
+$(function () {
+    $('.blue-line').each(function () {
+        var $win = $(window),
+            $winH = $win.height(),
+            $connect = $(this),
+            position = $connect.offset().top,
+            current = 0,
+            scroll;
+        $win.on('load scroll', function () {
+            scroll = $win.scrollTop();
+            current = (1 - (position - scroll) / $winH) * 2 * 100;
+            if (current > 99.9) {
+                current = 100;
+            }
+            if (scroll > position - $winH) {
+                $connect.css({ width: current + '%' });
+            }
+        });
+    });
+});
+
