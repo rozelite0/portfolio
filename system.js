aab84215ef79e743f140c0a9b5c4300580824397
rozelite0 <rozelite0@gmail.com>
2020-08-27 04:34:57 +0000

    update: ナビの修正・スキルの追加

--- system.js
+++ system.js
@@ -4,41 +4,35 @@ const MYWORK_WEREWOLF = document.getElementById('myWorkWerewolf');
 const MYWORK_WORDCOUNT = document.getElementById('myWorkWordCount');
 
 //ファンタジー創作キャラ製造機
-//ON
 MYWORK_FANTASY.addEventListener('mouseover', () => {
     APP_OVERVIEW.innerHTML = "構想からコーディングまで、初めて一人で制作し、無事に完成まで持っていくことができたWebアプリ。";
 }, false);
 
-//OUT
 MYWORK_FANTASY.addEventListener('mouseout', () => {
     APP_OVERVIEW.innerHTML = "";
 }, false);
 
 
 //人狼GMサポートWebアプリ
-//ON
 MYWORK_WEREWOLF.addEventListener('mouseover', () => {
     APP_OVERVIEW.innerHTML = "学校内で人狼ゲームのGMを務めることが多く、役職をもっと視覚的にメモできないか、と思ったのがきっかけで制作したWebアプリ。<br>制作期間は2日間。土日を利用して制作した。";
 }, false);
 
-//OUT
 MYWORK_WEREWOLF.addEventListener('mouseout', () => {
     APP_OVERVIEW.innerHTML = "";
 }, false);
 
 
 //文字カウント
-//ON
 MYWORK_WORDCOUNT.addEventListener('mouseover', () => {
     APP_OVERVIEW.innerHTML = "ただ文字数をカウントしてくれるだけのサイト。";
 }, false);
 
-//OUT
 MYWORK_WORDCOUNT.addEventListener('mouseout', () => {
     APP_OVERVIEW.innerHTML = "";
 }, false);
 
-//JQuery
+
 
 $(function() {
     //グローバルナビの座標を取得
@@ -72,7 +66,7 @@ $(function() {
     });
 });
 
-//スクロールすると画像も少し動く
+//Top image
 $(window).on('scroll', function() {
 
     var scrollTop = $(window).scrollTop();
